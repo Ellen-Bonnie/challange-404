ae0929ee279cd1769df3a96d32ce0537e0a60ad2
Ellen-Bonnie <ellen.nestyurina@gmail.com>
2021-09-16 22:41:31 +0300

    Update custom.js

--- custom.js
+++ custom.js
@@ -6,3 +6,10 @@
     $("#mobile-menu").toggleClass("open");
   });
 });
+(function() {
+  $("#bgBtn").click(function() {
+    // cssで背景色を切り替え
+    //toggleClassメソッドは 指定したクラス名の CSS がある場合は削除を行い、なければ追加する というメソッド
+    $(".bgBox").toggleClass("bgPink");
+  });
+});
