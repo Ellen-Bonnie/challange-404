a8cb266180c7e14697e459e5d606b844a6839635
Ellen-Bonnie <ellen.nestyurina@gmail.com>
2021-09-16 22:48:09 +0300

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,6 +1,6 @@
 
 (function() {
-  $("a#sidr").click(function() {
+  $("a#mobile-menu").click(function() {
     // cssで背景色を切り替え
     //toggleClassメソッドは 指定したクラス名の CSS がある場合は削除を行い、なければ追加する というメソッド
     $("#mobile-menu").toggleClass("open");
