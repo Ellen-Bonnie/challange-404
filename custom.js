a71edd5e5b5782dec690e8e52d6a4711dae74a6d
Ellen-Bonnie <ellen.nestyurina@gmail.com>
2021-09-17 15:42:29 +0300

    Update custom.js

--- custom.js
+++ custom.js
@@ -5,6 +5,6 @@ $(function() {
   $("#mobile-menu").click(function() {
     // cssで背景色を切り替え
     //toggleClassメソッドは 指定したクラス名の CSS がある場合は削除を行い、なければ追加する というメソッド
-    $("#dl-menuwrapper").toggleClass("active");
+    $(".dl-menuwrapper").toggleClass("active");
   });
 });
