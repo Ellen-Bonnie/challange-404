d9b27b7ce0688aa5eed8340bc9a27894d78cb02c
Ellen-Bonnie <ellen.nestyurina@gmail.com>
2021-09-17 21:09:41 +0300

    Update custom.js

--- custom.js
+++ custom.js
@@ -2,7 +2,7 @@ function toggleActive() {
 
 $('#mobile-menu').on('click touchend', function( event ) {
   event.preventDefault();
-  $('.dl-menuwrapper').toggleClass('active');
+  $('body').toggleClass('active');
 });
 }
 
