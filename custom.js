d7bf22353b2ced999e4efb7eb1f9f14ec03fa17a
Ellen-Bonnie <88135184+Ellen-Bonnie@users.noreply.github.com>
2023-08-06 15:44:09 +0300

    zen

--- custom.js
+++ custom.js
@@ -5,7 +5,3 @@ $('#mobile-menu').on('click touchend', function( event ) {
   $('body').toggleClass('active');
 });
 }
-
-<!-- Start of relokia5814 Zendesk Widget script -->
-<script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=c960580f-4f93-4671-8193-34673cd084b1"> </script>
-<!-- End of relokia5814 Zendesk Widget script -->
